053c5dcea1bd463cdb73f67029d5caa966901855
pvin <azri@azri-System-Product-Name.(none)>
2013-10-30 11:43:20 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrigamiHub::Application.config.secret_token = '3f11f7d0bcb4c6ead4a37129cf6cb5f66fda12ea9fb72e721bad8ebce5107e3f0547a0528046c8f542be24e065aebd90c223a2ced6de88a95372a924d7d422a5'
